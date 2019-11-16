e94f08d4db122b33d9fc47b8ec4cbdd72fc09745
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-11-16 14:47:25 -0500

    Rename readOnlyInputs to disabledInput
    Closes #2

--- src/bootstrap-input-spinner.js
+++ src/bootstrap-input-spinner.js
@@ -37,7 +37,7 @@
             boostThreshold: 10, // boost after these steps
             boostMultiplier: "auto", // you can also set a constant number as multiplier
             locale: null, // the locale for number rendering; if null, the browsers language is used
-            readOnlyInputs: false // make the text input disabled, while still allowing stepping
+            disabledInput: false // make the text input disabled, while still allowing stepping
         }
         for (var option in options) {
             config[option] = options[option]
@@ -47,7 +47,7 @@
             '<div class="input-group-prepend">' +
             '<button style="min-width: ' + config.buttonsWidth + '" class="btn btn-decrement ' + config.buttonsClass + '" type="button">' + config.decrementButton + '</button>' +
             '</div>' +
-            '<input type="text" style="text-align: ' + config.textAlign + '" class="form-control ' + config.inputClass + '"' + (config.readOnlyInputs ? " disabled" : "") + '/>' +
+            '<input type="text" style="text-align: ' + config.textAlign + '" class="form-control ' + config.inputClass + '"' + (config.disabledInput ? " disabled" : "") + '/>' +
             '<div class="input-group-append">' +
             '<button style="min-width: ' + config.buttonsWidth + '" class="btn btn-increment ' + config.buttonsClass + '" type="button">' + config.incrementButton + '</button>' +
             '</div>' +
@@ -174,7 +174,7 @@
             }
 
             function stepHandling(step) {
-                if (config.readOnlyInputs || (!$input[0].disabled && !$input[0].readOnly)) {
+                if (config.disabledInput || (!$input[0].disabled && !$input[0].readOnly)) {
                     calcStep(step)
                     resetTimer()
                     autoDelayHandler = setTimeout(function () {
@@ -224,11 +224,11 @@
                 // copy properties from original to the new input
                 $input.prop("required", $original.prop("required"))
                 $input.prop("placeholder", $original.prop("placeholder"))
-                var disabled = config.readOnlyInputs || $original.prop("disabled")
+                var disabled = config.disabledInput || $original.prop("disabled")
                 var readonly = $original.prop("readonly")
                 $input.prop("disabled", disabled)
                 $input.prop("readonly", readonly)
-                var isDisabled = (!config.readOnlyInputs && disabled) || readonly
+                var isDisabled = (!config.disabledInput && disabled) || readonly
                 var isIncrementDisabled = parseInt($input.val() || $original.val()) == parseInt($original.attr("max"))
                 var isDecrementDisabled = parseInt($input.val() || $original.val()) == parseInt($original.attr("min"))
                 $buttonIncrement.prop("disabled", isDisabled || isIncrementDisabled)
